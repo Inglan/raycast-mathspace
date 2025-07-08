b1db9b67c520d2beece73c21b8fa608f65ed43fe
Ingo Wolf <me@ingowolf.au>
2025-07-08 23:10:45 +1000

    show information

--- src/information.tsx
+++ src/information.tsx
@@ -1,5 +1,40 @@
 import { Detail } from "@raycast/api";
+import { useState } from "react";
+import { getBasicInformation, getLeaderboard } from "./mathspaceapi";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  const [basicData, setBasicData] = useState<{
+    name: { first: string; last: string };
+    email: string;
+    learningFocus: { id: string; name: string };
+  } | null>();
+
+  const [leaderboard, setLeaderboard] = useState<{
+    leaderboard: Array<{
+      points: number;
+      rank: number;
+      name: string;
+      avatarUrl: string;
+      studentId: string;
+      rankDelta: number;
+    }>;
+    xp: number;
+    place: number;
+  } | null>();
+
+  getLeaderboard().then(setLeaderboard);
+  getBasicInformation().then(setBasicData);
+
+  return (
+    <Detail
+      isLoading={basicData === undefined || leaderboard === undefined}
+      markdown={`# ${basicData?.name.first || ""} ${basicData?.name.last || ""}
+## Email: ${basicData?.email || ""}
+
+## XP: ${leaderboard?.xp || ""}
+
+## Place on leaderboard: ${leaderboard?.place || ""}
+    `}
+    />
+  );
 }
